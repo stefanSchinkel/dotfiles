e87b89fffd48788d542b71c570ff5833215acbb2
Stefan Schinkel <stefan.schinkel@gmail.com>
2015-01-13 12:05:24 +0100

    added script for regular backup to server (works @PIK only)

---
+++ bin/rdiffBackup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Crontab definition: 
+# backup every 30min from 9 to five, Tue to Fri
+# */30  9-17 * * 1-6   /home/schinkel/bin/rdiffBackup.sh > /dev/null
+rdiff-backup -v3 /home/schinkel/Downloads/ /pik/cluster/backup/Downloads/
+rdiff-backup -v3  /home/schinkel/temp/ /pik/cluster/backup/temp/
+rdiff-backup -v3   /home/schinkel/work/ /pik/cluster/backup/work/
