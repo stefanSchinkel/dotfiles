82f72010be21be4cffcdeeb57e0217c30d10e28f
Stefan Schinkel <mail@dreeg.org>
2015-01-15 11:57:08 +0100

    simple script to copy stuff

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -fsv ~/dotfiles/bashrc ~/.bashrc
+ln -fsv ~/dotfiles/coloursrc ~/.coloursrc
+cp -uv ~/dotfiles/bin/getGitBranch ~/bin/getGitBranch
+cp -uvR ~/dotfiles/bin/fortune ~/bin/
