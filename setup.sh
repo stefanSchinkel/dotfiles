e594c3b8af643da430ece893dc529166fc66c1e7
Stefan Schinkel <stefan.schinkel@gmail.com>
2015-02-17 14:28:14 +0100

    progres

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 
 ln -fsv ~/dotfiles/bashrc ~/.bashrc
 ln -fsv ~/dotfiles/coloursrc ~/.coloursrc
+ln -fsv ~/dotfiles/bookmarks.xml ~/.local/share/radiotray/bookmarks.xml 
+
 cp -uv ~/dotfiles/bin/getGitBranch ~/bin/getGitBranch
 cp -uvR ~/dotfiles/bin/fortune ~/bin/
