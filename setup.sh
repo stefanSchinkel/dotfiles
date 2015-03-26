a563aa657e83c8c2aa4a466002da0ec3fbc6a653
Stefan Schinkel <mail@dreeg.org>
2015-03-26 15:59:37 +0100

    JS[h|l]int & conky
    added JSLint & JSHint configs
    changed conky for dual-screen

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 ln -fsv ~/dotfiles/bashrc ~/.bashrc
 ln -fsv ~/dotfiles/coloursrc ~/.coloursrc
-ln -fsv ~/dotfiles/bookmarks.xml ~/.local/share/radiotray/bookmarks.xml 
+ln -fsv ~/dotfiles/bookmarks.xml ~/.local/share/radiotray/bookmarks.xml
+ln -fsv ~/dotfiles/jshintrc .jshintrc
+ln -fsv ~/dotfiles/jslintrc .jslintrc
 
 cp -uv ~/dotfiles/bin/getGitBranch ~/bin/getGitBranch
 cp -uvR ~/dotfiles/bin/fortune ~/bin/
