480d5e09d1796b5c03b9ebde16fcc4fee9b036ae
Stefan Schinkel <mail@dreeg.org>
2015-04-30 14:22:47 +0200

    add vimrc and setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 ln -fsv ~/dotfiles/bashrc ~/.bashrc
 ln -fsv ~/dotfiles/coloursrc ~/.coloursrc
+ln -fsv ~/dotfiles/vimrc ~/.vimrc
+
+
 ln -fsv ~/dotfiles/bookmarks.xml ~/.local/share/radiotray/bookmarks.xml
 ln -fsv ~/dotfiles/jshintrc .jshintrc
 ln -fsv ~/dotfiles/jslintrc .jslintrc
---
+++ vimrc
@@ -0,0 +1,7 @@
+autocmd BufReadPost COMMIT_EDITMSG exec "normal! gg"
+set number
+
+syntax enable
+set t_Co=256
+set background=dark
+colorscheme beekai
