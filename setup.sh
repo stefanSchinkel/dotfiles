865b3fe30b22dbbdd42ff186fd3539f61ea2847b
Stefan Schinkel <mail@dreeg.org>
2015-05-12 09:47:20 +0200

    Sublime Preferences

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ ln -fsv ~/dotfiles/jshintrc .jshintrc
 ln -fsv ~/dotfiles/jslintrc .jslintrc
 
 ln -fsv ~/dotfiles/sublime/Default\ \(Linux\).sublime-keymap ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymapln -s
-
+ln -fvs ~/dotfiles/sublime/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 
 cp -uv ~/dotfiles/bin/getGitBranch ~/bin/getGitBranch
 cp -uvR ~/dotfiles/bin/fortune ~/bin/
