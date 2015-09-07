bd6ee1624aa929cd0cca1e10eb217a11723fdfe3
Stefan Schinkel <mail@dreeg.org>
2015-09-07 13:51:11 +0200

    []

--- bin/fullbackup.sh
+++ bin/fullbackup.sh
@@ -3,4 +3,5 @@
 
 tar -cpf /pik/cluster/backup/TMP.tar --directory=$HOME . 2>  /dev/null
 mv /pik/cluster/backup/TMP.tar /pik/cluster/backup/HOME.tar
+#compression, not needed yet
 #pbzip2 -9 /pik/cluster/backup/HOME.tar
