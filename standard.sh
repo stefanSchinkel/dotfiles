3a2356c0ecdd9bfc0b07266b6967668f60f1f564
Stefan Schinkel <mail@dreeg.org>
2016-06-08 09:37:38 +0200

    [layout] 3 landscape screens agin

--- standard.sh
+++ standard.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1200 --pos 1024x80 --rotate normal --output HDMI2 --mode 1280x1024 --pos 2944x0 --rotate left --output HDMI1 --off --output eDP1 --off --output VGA1 --mode 1280x1024 --pos 0x0 --rotate left
+xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1200 --pos 1280x0 --rotate normal --output HDMI2 --mode 1280x1024 --pos 3200x176 --rotate normal --output HDMI1 --off --output eDP1 --off --output VGA1 --mode 1280x1024 --pos 0x176 --rotate normal
