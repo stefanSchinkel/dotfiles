36a2ef91e218298566e10625167a62e3f62cf313
Stefan Schinkel <mail@dreeg.org>
2018-11-23 10:30:47 +0100

    [screen] adjust monitor layout

--- screenlayout/upright_left.sh
+++ screenlayout/upright_left.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-1 --off --output DP-1 --mode 1920x1200 --pos 1200x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 1200x1200 --rotate normal --output HDMI-2 --mode 1920x1200 --pos 0x0 --rotate left
+xrandr --output HDMI-1 --off --output DP-1 --mode 1920x1200 --pos 0x0 --rotate left --output eDP-1 --primary --mode 1920x1080 --pos 3120x840 --rotate normal --output HDMI-2 --mode 1920x1200 --pos 1200x720 --rotate normal
