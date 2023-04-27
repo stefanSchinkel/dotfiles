f53defdfb7e4cc1fd6417add0f085ea0d8df3546
Stefan Schinkel <stefan@paxlife.aero>
2023-04-27 13:11:16 +0200

    cleanup 3/x

--- bin/fullbackup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# full backup of homedir 
-
-tar -cpf /pik/cluster/backup/TMP.tar --directory=$HOME . 2>  /dev/null
-mv /pik/cluster/backup/TMP.tar /pik/cluster/backup/HOME.tar
-#compression, not needed yet
-#pbzip2 -9 /pik/cluster/backup/HOME.tar
