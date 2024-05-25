0baa628882a2e6b290acbd53a4733cb55c746f04
Stefan Schinkel <mail@dreeg.org>
2024-05-25 21:23:43 +0200

    [setup.sh] start collecting stuff

--- setup.sh
+++ setup.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
 
-# configs
-ln -fsv ~/.dotfiles/bashrc ~/.bashrc
-ln -fsv ~/.dotfiles/tmux.conf ~/.tmux.conf
-
-# screen layouts
-# vim
-ln -fsv ~/.dotfiles/vimrc ~/.vimrc
-# ln -s ~/.vim/colors/zenburn.vim ~/.vim/bundle/Zenburn/colors/zenburn.vim
-
-
-# helper binaries
-# for $HELPER in bin; do foo; done
-ln -fsv ~/.dotfiles/bin/getGitBranch ~/bin/
-ln -fsv ~/.dotfiles/bin/fortune ~/bin/
-ln -fsv ~/.dotfiles/bin/matrix ~/bin/
-
+sudo apt-get install -y git make build-essential zsh vim tmux plocate wget curl mc meld python3-poetry \
+nextcloud-desktop keepassxc gnome-tweaks gnome-sushi guake chromium-browser silversearcher-ag fzf ripgrep
